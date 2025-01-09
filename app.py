d0cc6be8b00bdbcd332c2608ca6ef899c91df924
Bhavin Karangia <omkarpro6@gmail.com>
2025-01-09 21:32:08 +0530

    Formatted the output of app.py

--- app.py
+++ app.py
@@ -20,9 +20,9 @@ if st.button("PREDICT"):
     try:
         obj=prediction_pipeline.InputConverter(carat,depth,table,cut,color,clarity)
         predict_obj=prediction_pipeline.PridictionPipeline()
-        res = predict_obj.predict(obj.convert_to_Datafram())
+        res = predict_obj.predict(obj.convert_to_Datafram())[0]
         # st.write(type(res))
-        st.write(f"The price of the Gemstone/Diamond is {res[0]}$")
+        st.write(f"The price of the Gemstone/Diamond is {res}$")
     except Exception as e:
         logging.error(f"An error occurred: {str(e)}")
         raise CustomException(e,sys)
