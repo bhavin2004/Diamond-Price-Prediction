c48e6984f3c25c6692e60223d16fb5b90a22cba2
Bhavin <omkarpro6@gmail.com>
2025-03-20 09:05:58 +0530

    Added sample data button

--- app.py
+++ app.py
@@ -4,18 +4,53 @@ from src.pipelines import prediction_pipeline
 import streamlit as st
 import sys
 from src.pipelines import training_pipeline
+import pandas as pd
+from streamlit_option_menu import option_menu
+
+st.set_page_config("Diamond Price Prediction",page_icon='istockphoto-1152003770-612x612.jpg')
+# menu = option_menu()
+
 
 st.title("Diamond Price Prediction System")
+df=pd.read_csv('artifacts/test_data.csv')
+df=df.sample(6)
 
-carat=float(st.text_input("CARAT",0))
-depth=float(st.text_input("DEPTH",0))
-table=float(st.text_input("TABLE",0))
-cut=st.selectbox("CUT",['Fair', 'Good', 'Very Good', 'Premium', 'Ideal'])
-color=st.selectbox("COLOR",['D', 'E', 'F', 'G', 'H', 'I', 'J'])
-clarity=st.selectbox("CLARITY",['I1', 'SI2', 'SI1', 'VS2', 'VS1', 'VVS2', 'VVS1', 'IF'])
 
-st.balloons()
-    
+st.divider()
+
+#Giving Defualt Values
+carat=0
+depth=0
+table=0
+cut_list=['Fair', 'Good', 'Very Good', 'Premium', 'Ideal']
+cut=0
+color_list=['D', 'E', 'F', 'G', 'H', 'I', 'J']
+color=0
+clarity_list=['I1', 'SI2', 'SI1', 'VS2', 'VS1', 'VVS2', 'VVS1', 'IF']
+clarity=0
+
+sample=st.selectbox(label="Select The Sample Data",placeholder="Choose An Option",options=["Choose an option"] +[f"Sample Data {i}" for i in range(1,6)],index=0)
+i=sample[-1]
+sample_btn = st.button("Select")
+if sample_btn and i.isdigit():
+    i=int(i)
+    sample_data=df.iloc[i]
+    carat=sample_data['carat']
+    depth=sample_data['depth']
+    table=sample_data['table']
+    cut=cut_list.index(sample_data['cut'])
+    color=color_list.index(sample_data['color'])
+    clarity=clarity_list.index(sample_data['clarity'])
+    st.balloons()
+  
+carat=float(st.text_input("CARAT",carat))
+depth=float(st.text_input("DEPTH",depth))
+table=float(st.text_input("TABLE",table))
+cut=st.selectbox("CUT",cut_list,index=cut)
+color=st.selectbox("COLOR",color_list,index=color)
+clarity=st.selectbox("CLARITY",clarity_list,index=clarity)
+st.divider()
+
 if st.button("PREDICT"):
     # st.write("HI")
     try:
